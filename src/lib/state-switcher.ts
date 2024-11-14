8750c84766d76f198c69ac873e365687119b340a
fulcanelly <dallasxfulcanelly@gmail.com>
2024-11-15 01:57:56 +0200

    fut: fix

--- src/lib/state-switcher.ts
+++ src/lib/state-switcher.ts
@@ -3,6 +3,6 @@ import { switchState } from "./implicit_state"
 
 export const stateSwitcher: AllStates = new Proxy({} as any, {
   get(_, name: string) {
-    return args => switchState(name as any, args)
+    return args => (switchState as any)(name as any, args)
   }
 })
